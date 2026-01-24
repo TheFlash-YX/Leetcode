f6c897a4bf8195ebbe6bb61199c3a052769fb8e8
TheFlash-YX <1807648646@qq.com>
2026-01-24 11:18:50 +0800

    solved leetcode 216 组合总和Ⅲ

--- Backtrack/216.组合总和Ⅲ.py
+++ Backtrack/216.组合总和Ⅲ.py
@@ -2,9 +2,9 @@ from typing import List
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         result=[]
-        self.backtracking([],result,k,startindex=1,currentsum=0,targetsum=n)
+        self.backtracking(0,n,[],result,k,1)
         return result
-    def backtracking(self,path,result,k,startindex,currentsum,targetsum):
+    def backtracking(self,currentsum,targetsum,path,result,k,startindex):
         if currentsum > targetsum:
             return
         if len(path)==k:
@@ -12,9 +12,9 @@ class Solution:
                 result.append(path[:])
             return
 
-        for i in range(startindex,9-(k-len(path))+1+1):
+        for i in range(startindex,9):
             currentsum+=i
             path.append(i)
-            self.backtracking(path,result,k,i+1,currentsum,targetsum)
+            self.backtracking(currentsum,targetsum,path,result,k,i+1)
             currentsum-=i
             path.pop()
