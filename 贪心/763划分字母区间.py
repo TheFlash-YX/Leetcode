d03fe483eab8f214117a67420fba5f8c375dbbe5
TheFlash-YX <1807648646@qq.com>
2026-02-22 09:39:07 +0800

    2026/2/21

---
+++ 贪心/763划分字母区间.py
@@ -0,0 +1,17 @@
+from typing import List
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        last_occurence={}
+        for i,ch in enumerate(s):
+            last_occurence[ch]=i
+
+        result=[]
+        start=0
+        end=0
+        for i,ch in enumerate(s):
+            end=max(end,last_occurence[ch])
+            if i==end:
+                result.append(end-start+1)
+                start=i+1
+
+        return result
