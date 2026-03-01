2a334eee2771a943a8589b947fa10555ee5a3366
TheFlash-YX <1807648646@qq.com>
2026-03-01 10:44:04 +0800

    update solution of '42.Trap'

--- Hot100/DualPointer/42.Trap.py
+++ Hot100/DualPointer/42.Trap.py
@@ -1,12 +1,12 @@
 #接雨水
-
+from gettext import dpgettext
 from typing import List
 class Solution:
     #动态规划
     def trap1(self, height: List[int]) -> int:
         leftMax=[0]*len(height)
-        leftMax[0]=height[0]
         rightMax=[0]*len(height)
+        leftMax[0]=height[0]
         rightMax[-1]=height[-1]
 
         for i in range(1,len(height)):
@@ -15,15 +15,36 @@ class Solution:
             rightMax[i]=max(rightMax[i+1],height[i])
 
         dp=[0]*len(height)
-
         for i in range(len(height)):
             dp[i]=min(leftMax[i],rightMax[i])-height[i]
 
         return sum(dp)
 
+    #双指针
+    def trap2(self, height: List[int]) -> int:
+        left=0
+        right=len(height)-1
+        ans=0
+        leftMax=rightMax=0
+        while left<right:
+            leftMax = max(leftMax, height[left])
+            rightMax = max(rightMax, height[right])
+            if height[left]<height[right]:
+                ans+=leftMax-height[left]
+                left+=1
+            else:
+                ans+=rightMax-height[right]
+                right-=1
+
+        return ans
+
+
+
+
+
 
 if __name__=="__main__":
     solution=Solution()
-    height=[4,2,0,3,2,5]
-    result=solution.trap(height)
+    height=[0,1,0,2,1,0,1,3,2,1,2,1]
+    result=solution.trap2(height)
     print(result)
