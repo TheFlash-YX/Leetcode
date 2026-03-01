37845d88989a4075c7037959e7eaaf7423d4d1ab
TheFlash-YX <1807648646@qq.com>
2026-03-01 10:47:36 +0800

    test

--- Hot100/DualPointer/42.Trap.py
+++ Hot100/DualPointer/42.Trap.py
@@ -24,16 +24,16 @@ class Solution:
     def trap2(self, height: List[int]) -> int:
         left=0
         right=len(height)-1
-        ans=0
+        ans_=0
         leftMax=rightMax=0
         while left<right:
             leftMax = max(leftMax, height[left])
             rightMax = max(rightMax, height[right])
             if height[left]<height[right]:
-                ans+=leftMax-height[left]
+                ans_+=leftMax-height[left]
                 left+=1
             else:
-                ans+=rightMax-height[right]
+                ans_+=rightMax-height[right]
                 right-=1
 
         return ans
