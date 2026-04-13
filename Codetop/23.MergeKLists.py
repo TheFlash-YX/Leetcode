863b4f1f9994d92fa0cc55713327cf885d686ca9
TheFlash-YX <1807648646@qq.com>
2026-04-13 11:44:03 +0800

    update

---
+++ Codetop/23.MergeKLists.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+from typing import List, Optional
+import heapq
+class ListNode:
+    def __init__(self,val=0,next=None):
+        self.val=val
+        self.next=next
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        dummy = ListNode()
+        cur = dummy
+        heap = []
+
+        for i, node in enumerate(lists):
+            if node:
+                heapq.heappush(heap, (node.val, i, node))
+
+        while heap:
+            val, i, node = heapq.heappop(heap)
+            cur.next = node
+            cur = cur.next
+            if node.next:
+                heapq.heappush(heap, (node.next.val, i, node.next))
+
+        return dummy.next
+
+
